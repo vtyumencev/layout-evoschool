555d9dfdfa52ed59ac2a96a26ae280948f0e014a
Vyacheslav <tvo0@ya.ru>
2021-08-10 17:43:07 +0500

    fix 2

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 const { src, dest, watch, parallel, series }  = require('gulp');
 
-const scss          = require('gulp-sass');
-const concat        = require('gulp-concat');
-const browserSync   = require('browser-sync').create();
-const uglify        = require('gulp-uglify-es').default;
-const autoprefixer  = require('gulp-autoprefixer');
-const imagemin      = require('gulp-imagemin');
-const del           = require('del');
+const scss              = require('gulp-sass');
+const concat            = require('gulp-concat');
+const browserSync       = require('browser-sync').create();
+const uglify            = require('gulp-uglify-es').default;
+const autoprefixer      = require('gulp-autoprefixer');
+const imagemin          = require('gulp-imagemin');
+const del               = require('del');
+
 
 function browsersync() {
   browserSync.init({
@@ -22,20 +23,20 @@ function cleanDist() {
 
 function images() {
   return src('app/images/**/*.{jpg,jpeg,png,gif,ico}')
-    // .pipe(imagemin(
-    //   [
-    //     imagemin.gifsicle({ interlaced: true }),
-    //     imagemin.mozjpeg({ quality: 75, progressive: true }),
-    //     imagemin.optipng({ optimizationLevel: 5 }),
-    //     imagemin.svgo({
-    //       plugins: [
-    //         { removeViewBox: true },
-    //         { cleanupIDs: false }
-    //       ]
-    //     })
-    //   ]
-    // ))
-    .pipe(dest('app/images'))
+    .pipe(imagemin(
+      [
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.mozjpeg({ quality: 75, progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 }),
+        imagemin.svgo({
+          plugins: [
+            { removeViewBox: true },
+            { cleanupIDs: false }
+          ]
+        })
+      ]
+    ))
+    .pipe(dest('dist/images'))
 }
 
 function scripts() {
@@ -82,15 +83,22 @@ function watching() {
   watch(['app/*.html']).on('change', browserSync.reload);
 }
 
+function cssUrls() {
+    return src('app/css/style.min.css')
+        .pipe(cssAdjustUrlPath(/(url\(['"]?)[/]?(assets)/g))
+        .pipe(dest('./'));
+}
+
 exports.styles = styles;
 exports.watching = watching;
 exports.browsersync = browsersync;
 exports.scripts = scripts;
 exports.images = images;
 exports.cleanDist = cleanDist;
+exports.cssUrls = cssUrls;
 
 
 exports.build = series(cleanDist, images, build);
-exports.default = parallel(styles ,scripts ,browsersync, watching);
+exports.default = parallel(styles, scripts, browsersync, watching);
 
 
